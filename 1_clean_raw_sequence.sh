289e8fe2cff15791f6e1a0f5238518610911e8d3
100dru <63719674+100dru@users.noreply.github.com>
2025-01-10 16:18:34 -0500

    Update 1_clean_raw_sequence.sh

--- 1_clean_raw_sequence.sh
+++ 1_clean_raw_sequence.sh
@@ -7,7 +7,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shatadru.2@osu.edu
 
-## here filename=${i}; for i in `ls -1 *_R1_001.fastq.gz | sed 's/_R1_001.fastq.gz//'`
+## here filename=${i}. the terminal code is:
+
+#for i in `ls -1 *_R1_001.fastq.gz | sed 's/_R1_001.fastq.gz//'`
+#do sbatch --export=filename=${i} clean.sh
+#done
 
 /fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/bbduk.sh -Xmx48G threads=12 overwrite=t in=${filename}_R1_001.fastq.gz in2=${filename}_R2_001.fastq.gz ref=/fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/resources/adapters.fa,/fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/resources/phix174_ill.ref.fa.gz ktrim=r minlength=30 k=23 mink=11 hdist=1 hdist2=1 out=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R1_no_adapter_phix.fastq.gz out2=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R2_no_adapter_phix.fastq.gz refstats=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_no_adapter_phix_Trimming.stats statscolumns=5
 
