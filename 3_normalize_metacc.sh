5da4693edf69bd2cc85dcb55ee7a9d7e594cadf6
100dru <63719674+100dru@users.noreply.github.com>
2025-01-09 20:51:16 -0500

    Create 3_normalize_metacc.sh

---
+++ 3_normalize_metacc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --account=PAS1117
+#SBATCH --job-name=metacc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=shatadru.2@osu.edu
+
+module use /fs/ess/PAS1117/modulefiles
+module load MetaCC
+
+cd /fs/ess/PAS1117/HiC/6thRoundTests/HiC
+
+MetaCC norm -e Sau3AI -e MluCI NewHiCRef.fasta ${filename}_MAP_SORTED.bam /fs/ess/PAS1117/HiC/6thRoundTests/HiC/metacc/${filename}_out
