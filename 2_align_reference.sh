c4bc515f8ed56532143d7192f6651e7f4dea4380
100dru <63719674+100dru@users.noreply.github.com>
2025-01-09 20:47:25 -0500

    Create 2_align_reference.sh

---
+++ 2_align_reference.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=bwa
+#SBATCH --account=PAS1117
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=08:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=shatadru.2@osu.edu
+
+
+module use /fs/project/PAS1117/modulefiles
+module load bwa/0.7.17-r1198
+module load samtools/1.10
+
+cd /fs/ess/PAS1117/HiC/6thRoundTests/HiC 
+
+#bwa index NewHiCRef.fasta
+
+bwa mem -5SP -T0 -t16 NewHiCRef.fasta /fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R1_clean.fastq.gz /fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R2_clean.fastq.gz -o ${filename}_aligned.sam
+
+
+samtools view -F 0x904 -bS ${filename}_aligned.sam > ${filename}_MAP_UNSORTED.bam
+samtools sort -n ${filename}_MAP_UNSORTED.bam -o ${filename}_MAP_SORTED.bam
+
