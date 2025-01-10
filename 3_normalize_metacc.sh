72a58e89b4086304e5696c494967b7b7208540be
100dru <63719674+100dru@users.noreply.github.com>
2025-01-10 16:20:33 -0500

    Update 3_normalize_metacc.sh

--- 3_normalize_metacc.sh
+++ 3_normalize_metacc.sh
@@ -12,5 +12,10 @@ module load MetaCC
 
 cd /fs/ess/PAS1117/HiC/6thRoundTests/HiC
 
-### here filename=${i}; for i in `ls -1 *_MAP_SORTED.bam | sed 's/_MAP_SORTED.bam//'`
+
+## here filename=${i}. the terminal code is:
+
+#for i in `ls -1 *_MAP_SORTED.bam | sed 's/_MAP_SORTED.bam//'`
+#do sbatch --export=filename=${i} bwa_mock.sh
+#done
 MetaCC norm -e Sau3AI -e MluCI NewHiCRef.fasta ${filename}_MAP_SORTED.bam /fs/ess/PAS1117/HiC/6thRoundTests/HiC/metacc/${filename}_out
