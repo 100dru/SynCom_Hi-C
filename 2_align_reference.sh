6c0d74e28afc331ad37eae54ef9209a1f0f34cab
100dru <63719674+100dru@users.noreply.github.com>
2025-03-26 19:22:53 -0400

    Update 2_align_reference.sh

--- 2_align_reference.sh
+++ 2_align_reference.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 #SBATCH --job-name=bwa
-#SBATCH --account=PAS1117
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=28
 #SBATCH --time=08:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=shatadru.2@osu.edu
+#SBATCH --mail-user=your_email@example.com # Replace with your email
 
-## here filename=${i}. the terminal code is:
+## Define variables
+REFERENCE="NewHiCRef.fasta"
+READS_DIR="/path/to/clean_reads" # Replace with your reads directory
+OUTPUT_DIR="/path/to/output"     # Replace with your output directory
 
-#for i in `ls -1 *_R1_clean.fastq.gz | sed 's/_R1_clean.fastq.gz//'`
-#do sbatch --export=filename=${i} bwa_mock.sh
-#done
-
-module use /fs/project/PAS1117/modulefiles
+# Load necessary modules
 module load bwa/0.7.17-r1198
 module load samtools/1.10
 
-cd /fs/ess/PAS1117/HiC/6thRoundTests/HiC 
+# Change to the working directory
+cd $OUTPUT_DIR
 
-bwa index NewHiCRef.fasta
+# Index the reference genome
+bwa index $REFERENCE
 
-bwa mem -5SP -T0 -t16 NewHiCRef.fasta /fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R1_clean.fastq.gz /fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R2_clean.fastq.gz -o ${filename}_aligned.sam
+# Align the reads to the reference genome
+bwa mem -5SP -T0 -t16 $REFERENCE ${READS_DIR}/${filename}_R1_clean.fastq.gz ${READS_DIR}/${filename}_R2_clean.fastq.gz > ${filename}_aligned.sam
 
-
+# Convert SAM to BAM and sort
 samtools view -F 0x904 -bS ${filename}_aligned.sam > ${filename}_MAP_UNSORTED.bam
 samtools sort -n ${filename}_MAP_UNSORTED.bam -o ${filename}_MAP_SORTED.bam
 
+# Optional: Remove intermediate files to save space
+# rm ${filename}_aligned.sam ${filename}_MAP_UNSORTED.bam
