ff9a8491a26bba75e79f54d71b1c03dc545977fd
100dru <63719674+100dru@users.noreply.github.com>
2025-03-26 19:21:47 -0400

    Update 1_clean_raw_sequence.sh

--- 1_clean_raw_sequence.sh
+++ 1_clean_raw_sequence.sh
@@ -2,17 +2,20 @@
 #SBATCH -t 8:00:00
 #SBATCH -N 1
 #SBATCH -n 24
-#SBATCH -A PAS1117
+#SBATCH -A <account_name>
 #SBATCH -J cleaning_hic
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=shatadru.2@osu.edu
+#SBATCH --mail-user=<your_email>
 
-## here filename=${i}. the terminal code is:
+## Here filename=${i}. The terminal code is:
 
 #for i in `ls -1 *_R1_001.fastq.gz | sed 's/_R1_001.fastq.gz//'`
 #do sbatch --export=filename=${i} clean.sh
 #done
 
-/fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/bbduk.sh -Xmx48G threads=12 overwrite=t in=${filename}_R1_001.fastq.gz in2=${filename}_R2_001.fastq.gz ref=/fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/resources/adapters.fa,/fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/resources/phix174_ill.ref.fa.gz ktrim=r minlength=30 k=23 mink=11 hdist=1 hdist2=1 out=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R1_no_adapter_phix.fastq.gz out2=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R2_no_adapter_phix.fastq.gz refstats=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_no_adapter_phix_Trimming.stats statscolumns=5
+bbduk_path="<path_to_bbduk>"
+bbduk_ref_path="<path_to_reference_files>"
 
-/fs/ess/PAS1117/bioinformatic_tools/bbmap_38.51/bbduk.sh -Xmx48G threads=12 overwrite=t in=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R1_no_adapter_phix.fastq.gz in2=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R2_no_adapter_phix.fastq.gz  qtrim=rl maq=20 maxns=0 minlength=30 trimq=20 out=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R1_clean.fastq.gz  out2=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_R2_clean.fastq.gz outs=/fs/ess/PAS1117/HiC/6thRoundTests/HiC/clean_reads/${filename}_S_clean.fastq.gz
+$bbduk_path/bbduk.sh -Xmx48G threads=12 overwrite=t in=${filename}_R1_001.fastq.gz in2=${filename}_R2_001.fastq.gz ref=$bbduk_ref_path/adapters.fa ktrim=r k=23 mink=11 hdist=1 tpe tbo qtrim=rl trimq=10 minlen=20 out=${filename}_R1_clean.fastq.gz out2=${filename}_R2_clean.fastq.gz stats=${filename}_clean_stats.txt
+
+$bbduk_path/bbduk.sh -Xmx48G threads=12 overwrite=t in=${filename}_R1_clean.fastq.gz in2=${filename}_R2_clean.fastq.gz ref=$bbduk_ref_path/phix174_ill.ref.fa k=31 hdist=1 stats=${filename}_phix_stats.txt out=${filename}_R1_no_adapter_phix.fastq.gz out2=${filename}_R2_no_adapter_phix.fastq.gz
